90e88d6b4f5170c58f43d3d12c56cf07061a3f3b
3425 <3425@example.com>
2023-10-06 14:16:22 +0900

    ログイン機能に必要な要素の定義、userの操作

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+    
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to profile_path(@user)
+        else
+            render 'new'
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        redirect_to signup_path
+    end
+
+    pravete
+        def user_params
+            params.require(:user).permit(:email, :password, :password_confirmation, :name)
+        end
+end
