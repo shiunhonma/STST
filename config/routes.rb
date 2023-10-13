f8ef0f43ac6fa7441f079db1b17590e072a4e708
3425 <3425@example.com>
2023-10-13 14:34:07 +0900

    homeコントローラー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :chats
   resources :user_rooms
   resources :rooms
@@ -12,4 +13,5 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
 end
