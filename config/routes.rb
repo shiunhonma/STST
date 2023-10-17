fde150089caba434a1403136484e9dae8118c806
3425 <3425@example.com>
2023-10-17 09:56:38 +0900

    ルート指定、ルートをログイン画面に設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do  
   get 'home/index'
+  root to: 'home#index'
   resources :chats
   resources :user_rooms
   resources :rooms
@@ -7,6 +8,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+
   get '/profile', to: 'users#show'
   delete '/unsubscribe', to: 'users#destroy'
 
