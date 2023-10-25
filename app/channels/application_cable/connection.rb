dd2c90db7587b92368e752088dcf402d95225632
3425 <3425@example.com>
2023-10-25 12:46:20 +0900

    チャット機能エラー

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -7,7 +7,7 @@ module ApplicationCable
 
     protected
     def find_verified_user
-      if find_verified_user = User.find_by(id: cookies.signed[:user_id])
+      if verified_user = User.find_by(id: cookies.signed[:user_id])
         verified_user
       else
         reject_unauthorized_connection
--- app/models/chat.rb
+++ app/models/chat.rb
@@ -3,5 +3,5 @@ class Chat < ApplicationRecord
     belongs_to :user, optional: true
     belongs_to :room, optional: true
 
-    after_create_commit { ChatBroadcastJob.perform_later self}
+    after_create_commit { ChatBroadcastJob.perform_later self }
 end
