2a9a4e940fe77a11073b2e374773baa50ed76016
3425 <3425@example.com>
2023-10-27 15:03:54 +0900

    チャット機能、エンターキープッシュで送信、作成部屋と送る部屋の名前を一致させた

--- app/jobs/chat_broadcast_job.rb
+++ app/jobs/chat_broadcast_job.rb
@@ -3,7 +3,7 @@ class ChatBroadcastJob < ApplicationJob
 
   def perform(data)
     # Do something later
-    ActionCable.server.broadcast "room_channel_#{data.room_id}", message: render_message(data)
+    ActionCable.server.broadcast "some_channel_#{data.room_id}", message: render_message(data)
   end
 
   private
