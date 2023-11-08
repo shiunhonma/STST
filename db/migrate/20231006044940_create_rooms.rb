cd7262f575943f21f42cef4b9ecbbd1578c4b75e
3425 <3425@example.com>
2023-11-08 12:20:55 +0900

    部屋選択設定

--- db/migrate/20231006044940_create_rooms.rb
+++ db/migrate/20231006044940_create_rooms.rb
@@ -2,7 +2,7 @@ class CreateRooms < ActiveRecord::Migration[6.1]
   def change
     create_table :rooms do |t|
       t.string :room_name
-      t.integer :room_category
+      t.integer :room_category_id
       t.integer :user_id
 
       t.timestamps
