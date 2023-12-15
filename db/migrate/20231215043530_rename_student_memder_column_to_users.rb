6b220ceb1e8fe6f6a83f7c8be61237dbbe3aa07d
3425 <3425@example.com>
2023-12-15 13:56:42 +0900

    *

--- db/migrate/20231215043530_rename_student_memder_column_to_users.rb
+++
@@ -1,5 +0,0 @@
-class RenameStudentMemderColumnToUsers < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :users, :student_memder,:student_member
-  end
-end
--- db/migrate/20231215043709_rename_teacher_memder_column_to_users.rb
+++
@@ -1,5 +0,0 @@
-class RenameTeacherMemderColumnToUsers < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :users, :teacher_memder,:teacher_member
-  end
-end
